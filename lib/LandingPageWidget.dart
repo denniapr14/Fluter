4fbb93a35362c0bee3695a6a55644e78dc027036
Deni Aprianto <58542020+denniapr14@users.noreply.github.com>
2024-05-08 12:38:49 +0700

    upload tampilan

--- lib/LandingPageWidget.dart
+++ lib/LandingPageWidget.dart
@@ -11,95 +11,102 @@ class _LandingPageWidgetState extends State<LandingPageWidget> {
   String _selectedOption3 = '1 Milyar';
 
   @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900], // Set background color to dark grey
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Find Fast What You Need',
-              style: TextStyle(fontSize: 24, color: Colors.white),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/1261728/pexels-photo-1261728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Find Fast What You Need',
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  width: 700,
+                  height: 60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      DropdownButton<String>(
+                        value: _selectedOption1,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _selectedOption1 = newValue!;
+                          });
+                        },
+                        items: <String>['Greenland', 'Kalm', 'Joyo Grand']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                      SizedBox(width: 40),
+                      DropdownButton<String>(
+                        value: _selectedOption2,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _selectedOption2 = newValue!;
+                          });
+                        },
+                        items: <String>['100 jt', '200 jt', '400 jt']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                      SizedBox(width: 40),
+                      DropdownButton<String>(
+                        value: _selectedOption3,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _selectedOption3 = newValue!;
+                          });
+                        },
+                        items: <String>['1 Milyar', '2 Milyar', '4 Milyar']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                      SizedBox(width: 40),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Add button press logic here
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.black,
+                        ),
+                        child: Text('Search'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(40), 
-              ),
-              width: 700,
-              height: 60,
-              
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  DropdownButton<String>(
-                    value: _selectedOption1,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _selectedOption1 = newValue!;
-                      });
-                    },
-                    items: <String>['Greenland', 'Kalm', 'Joyo Grand']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    
-                  ),
-                  SizedBox(width: 40),
-                  DropdownButton<String>(
-                    value: _selectedOption2,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _selectedOption2 = newValue!;
-                      });
-                    },
-                    items: <String>['100 jt', '200 jt', '400 jt']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    
-                  ),
-                  SizedBox(width: 40),
-                  DropdownButton<String>(
-                    value: _selectedOption3,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _selectedOption3 = newValue!;
-                      });
-                    },
-                    items: <String>['1 Milyar', '2 Milyar', '4 Milyar']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    
-                  ),
-                  SizedBox(width: 40),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Add button press logic here
-                    },
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.black,
-                    ),
-                    child: Text('Search'),
-                  ),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
