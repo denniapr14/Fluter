b69e4fe6541b42ab795981bf4376e560fbd346ae
Deni Aprianto <58542020+denniapr14@users.noreply.github.com>
2024-06-14 11:07:06 +0700

    update

--- lib/logic/data/Color.dart
+++ lib/logic/data/Color.dart
@@ -1,10 +1,2 @@
 import 'package:flutter/material.dart';
 
-class AppColors {
-  static const Color color1 = Color(0xFF414037);
-  static const Color color2 = Color(0xFFC0BA84);
-  static const Color color3 = Color(0xFFEBDF7A);
-  static const Color color4 = Color(0xFF6B6954);
-  static const Color color5 = Color(0xFFFFEF5A);
-  static const Color color6 = Color(0xFFFFEC31);
-}
